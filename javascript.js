0276dbcac3f262eb58b3dde3fe09eccb8d0885e9
Maciej15PL <70692418+Maciej15PL@users.noreply.github.com>
2020-11-05 09:27:45 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function zmien(){
+    document.getElementById('dane').style.display="none";
+	document.getElementById('formularz2').style.display="inline";
+	document.querySelector('formularz2');
+}
+function zmien2(){
+	document.getElementById('formularz2').style.display="none";
+	document.getElementById('podziekowanie').style.display="inline";
+	
+}
